6c80958342109ae557c79c99520ed7f630de7341
James Stanley <james@incoherency.co.uk>
2022-07-17 15:51:02 +0100

    Write hello world to uart

--- src/hello.c
+++ src/hello.c
@@ -2,10 +2,12 @@
 #include "pico/stdlib.h"
 
 int main() {
+    stdio_usb_init();
     int pin = PICO_DEFAULT_LED_PIN;
     gpio_init(pin);
     gpio_set_dir(pin, GPIO_OUT);
     while (1) {
+        printf("Hello, world!\n");
         gpio_put(pin, 1);
         sleep_ms(250);
         gpio_put(pin, 0);
